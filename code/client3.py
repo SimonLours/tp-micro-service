7e2ddafa7e0d4b6281ff36a381a53c29fdaf381b
AxelPedrero1 <pedreroa@3il.fr>
2025-06-10 15:30:25 +0200

    Create client3.py

---
+++ code/client3.py
@@ -0,0 +1,14 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("172.16.20.139", 63000))  
+
+while True:
+    msg = input("Message à envoyer : ")
+    client.send(msg.encode())
+    if msg == "fin":
+        break
+    reponse = client.recv(1024).decode()
+    print("Serveur renvoie :", reponse)
+
+client.close()
