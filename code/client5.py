2f4a2be574ec3a4f1815e0d65ece649d555a42ed
AxelPedrero1 <pedreroa@3il.fr>
2025-06-10 16:41:45 +0200

    Update client5.py

--- code/client5.py
+++ code/client5.py
@@ -4,11 +4,13 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("172.16.20.139", 63000))
 
 while True:
-    expr = input("Entrez une expression à calculer : ")
+    expr = input("Entrez une expression à calculer (ou 'fin' pour quitter) : ")
     client.send(expr.encode())
-    if expr == "fin":
+
+    if expr.lower() == "fin":
         break
-    result = client.recv(1024).decode()
-    print("Résultat :", result)
+
+    resultat = client.recv(1024).decode()
+    print("Résultat :", resultat)
 
 client.close()
