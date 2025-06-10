387dc5555d40037265e2d4cab7498207e6d1e8fe
AxelPedrero1 <pedreroa@3il.fr>
2025-06-10 15:05:03 +0200

    Update client2.py

--- code/client2.py
+++ code/client2.py
@@ -1,7 +1,7 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("172.16.20.139", 63000))  # Remplace par l'IP de ton serveur
+client.connect(("172.16.20.139", 63000))  
 
 while True:
     msg = input("Message à envoyer : ")
